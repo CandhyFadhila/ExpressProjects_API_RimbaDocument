b44e786250b52ed6315bb35b32482cd24da9daf1
CandhyFadhila <candhy.fadhila.arsyad@gmail.com>
2025-10-21 07:46:24 +0700

    SETUP NEW LOCAL VPS

--- src/helpers/documentHelper.js
+++ src/helpers/documentHelper.js
@@ -13,7 +13,7 @@ function isLinux() {
 
 function resolvePublicBaseUrl(port = 4001) {
   return isLinux()
-    ? "https://doc.rimbaexium.org:3001"
+    ? "https://doc.rimbaexium.org"
     : `http://localhost:${port}`;
 }
 
